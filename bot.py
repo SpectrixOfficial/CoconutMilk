f68e93750fdbe50e753257b2f9753c8d6c3024cc
Brahm <33741267+notBanii@users.noreply.github.com>
2018-08-15 09:27:35 -0600

    have Modules, and finished up making code look prettier

--- bot.py
+++ bot.py
@@ -7,12 +7,21 @@ modules = [
 
 ]
 
+@bot.event
+async def on_ready():
+    print(f"{bot.config['name']} {bot.config['version']}")
+
+
 def main():
 
     with open("config.json") as f:
         bot.config = json.load(f)
 
     for module in modules:
-        bot.load_extension(module)
+        bot.load_extension(f'Modules.{module}')
 
     bot.run(bot.config["app"]["token"])
+
+
+if __name__ == '__main__':
+    main()
